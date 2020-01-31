9aae6e95766a42cc7d8f5e2426691c86c1369dea
Alex Fedotov <alxfed@gmail.com>
2020-01-31 08:50:12 -0600

    initial

---
+++ module.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""test module for colab
+"""
+import sqlalchemy as sqlalc
+
+
+def main():
+    print('main-done')
+    return
+
+
+if __name__ == '__main__':
+    main()
+    print('module done')
